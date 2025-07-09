591aed28c36fc2a335766190ed0b56636187cae2
suwon82 <suwon82@gsretail.com>
2025-07-09 10:01:46 +0900

    Initial commit

---
+++ src/main/java/com/event/msalearningproject/MsaLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.event.msalearningproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaLearningProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaLearningProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/event/msalearningproject/MsaLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.msalearningproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaLearningProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
